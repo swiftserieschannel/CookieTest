d072963f08eab520eed1e5b5642cdaa960eb5ed6
Chandra Bhushan <swiftserieschannel@gmail.com>
2020-07-24 11:08:18 +0530

    CookieTest
    CookieTest

--- CookieTest/ViewController.swift
+++ CookieTest/ViewController.swift
@@ -7,14 +7,72 @@
 //
 
 import UIKit
-
+import WebKit
 class ViewController: UIViewController {
-
+    var webView:WKWebView!
+    var customRequest: URLRequest!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let url = URL(string: "https://awein.000webhostapp.com/cookiestest.php")!
+        self.customRequest = URLRequest(url: url)
+        
+        addWebView()
     }
-
-
+    
+    private func addWebView(){
+        
+        let preferences = WKPreferences()
+        preferences.javaScriptEnabled = true
+        preferences.javaScriptCanOpenWindowsAutomatically = true
+        
+        let cookie = HTTPCookie(properties: [
+            .domain: "awein.000webhostapp.com",
+            .path: "/",
+            .name: "test",
+            .value: "value",
+            .secure: "TRUE",
+            .expires: NSDate(timeIntervalSinceNow: 3600)
+        ])
+        
+        //Makes sure the cookie is set before instantiating the webview and initiating the request
+        if let myCookie = cookie {
+            WKWebViewConfiguration.includeCookie(cookie: myCookie, preferences: preferences, completion: {
+                [weak self] config in
+                if let `self` = self {
+                    if let configuration = config {
+                        self.webView = WKWebView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width , height: self.view.frame.height), configuration: configuration)
+                        
+                        self.view.addSubview(self.webView)
+                        self.webView.load(self.customRequest)
+                        self.webView.allowsBackForwardNavigationGestures = true
+                    }
+                }
+            })
+        }
+    }
 }
 
+extension WKWebViewConfiguration {
+    
+    static func includeCookie(cookie:HTTPCookie, preferences:WKPreferences, completion: @escaping (WKWebViewConfiguration?) -> Void) {
+        let config = WKWebViewConfiguration()
+        config.preferences = preferences
+        
+        let dataStore = WKWebsiteDataStore.nonPersistent()
+        
+        DispatchQueue.main.async {
+            let waitGroup = DispatchGroup()
+            
+            waitGroup.enter()
+            dataStore.httpCookieStore.setCookie(cookie) {
+                waitGroup.leave()
+            }
+            
+            waitGroup.notify(queue: DispatchQueue.main) {
+                config.websiteDataStore = dataStore
+                completion(config)
+            }
+        }
+    }
+}
